b3348a42f1da2587c704d11cb58d9da1d86c834e
Jesús Adrián Ramírez López <jadrianramirezl@gmail.com>
2018-03-07 20:36:38 -0600

    Configuración para primer orden

--- src/functions/conekta.ts
+++ src/functions/conekta.ts
@@ -5,5 +5,36 @@ conekta.api_key = process.env.CONEKTA_API_KEY;
 conekta.api_version = '2.0.0';
 
 export async function createOxxoCharge(event, context, callback) {
-  console.log(conekta);
+  const order = conekta.Order.create({
+    "line_items": [{
+      "name": "Tacos",
+      "unit_price": 1000,
+      "quantity": 12
+    }],
+    "shipping_lines": [{
+      "amount": 1500,
+      "carrier": "FEDEX"
+    }], //shipping_lines - phyiscal goods only
+    "currency": "MXN",
+    "customer_info": {
+      "name": "Fulanito Pérez",
+      "email": "jadrianramirezl@gmail.com",
+      "phone": "+5218181818181"
+    },
+    "shipping_contact": {
+      "address": {
+        "street1": "Calle 123, int 2",
+        "postal_code": "06100",
+        "country": "MX"
+      }
+    }, //shipping_contact - required only for physical goods
+    "charges": [{
+      "payment_method": {
+        "type": "oxxo_cash"
+      }
+    }]
+  }, function (err, res) {
+    console.log(res.toObject());
+    return callback(null, success('success'));
+  });
 }
