4a00f7c8c96da8f5fd6a27dfc8132fd31b6db655
Silvester Garcia <ing.silvestre.garcia@gmail.com>
2018-03-21 20:55:26 -0600

    Se agrega funcionalidad de aws ses

--- src/functions/demo.ts
+++ src/functions/demo.ts
@@ -1,7 +1,10 @@
 "use strict";
 import { success, failure, notAllowed, redirect } from "./../libs/response-lib";
+import { resolve } from "url";
 
 var paypal = require("paypal-rest-sdk");
+var AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
 
 export async function demo(event, context, callback) {
   console.log("Hola mundo");
@@ -116,6 +119,19 @@ export async function onPaypalResult(event, context, callback) {
     );
     // console.log(payment);
     //return callback(null, success(payment));
+    var sns = new AWS.SNS({ apiVersion: "2010-03-31" });
+    var params = {
+      Message: "PayPal Payment ID XXX",
+      Subject: "Subject",
+      TopicArn: process.env.PAYPAL_PURCHASE_SNS
+    };
+    await new Promise((resolve, reject) =>
+      sns.publish(params, function(err, data) {
+        if (err)
+          reject(err); // an error occurred
+        else resolve(data); // successful response
+      })
+    );
     return callback(null, redirect(process.env.PAYPAL_PURCHASE_SUCCESS_URL));
   } catch (error) {
     // console.log(error);
@@ -126,5 +142,41 @@ export async function onPaypalResult(event, context, callback) {
 
 export async function onPayPalPurchaseSNS(event, context, callback) {
   console.log(event);
-  return callback(null, success({ event }));
+  var ses = new AWS.SES({ apiVersion: "2010-12-01" });
+  try {
+    var params = {
+      Destination: {
+        ToAddresses: ["ing.silvestre.garcia@gmail.com"]
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data:
+              'This message body contains HTML formatting. It can, for example, contain links like this one: <a class="ulink" href="http://docs.aws.amazon.com/ses/latest/DeveloperGuide" target="_blank">Amazon SES Developer Guide</a>.'
+          },
+          Text: {
+            Charset: "UTF-8",
+            Data: "This is the message body in text format."
+          }
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Test email"
+        }
+      },
+      Source: "ing.silvestre.garcia@gmail.com"
+    };
+    const email = await new Promise((resolve, reject) =>
+      ses.sendEmail(params, function(err, data) {
+        if (err) {
+          console.log(err);
+          reject(err); // an error occurred
+        } else resolve(data); // successful response
+      })
+    );
+    return callback(null, success({ email }));
+  } catch (error) {
+    return callback(null, error({ error }));
+  }
 }
